5cc7c9c000fd488afe5cc724ea8d92f46aee5b85
m-baqir <n01307731@humbermail.ca>
2021-10-20 20:36:15 -0400

    pivot table done

--- DjangoHouse/Listing/views.py
+++ DjangoHouse/Listing/views.py
@@ -13,18 +13,22 @@ def index(request):
     listings = Listing.objects.order_by('Date')
     return render(request, "index.html", {'listings': listings})
 
-def TorontoMapApi (request):
-    response = requests.get('https://ckan0.cf.opendata.inter.prod-toronto.ca/ne/api/3/action/datastore_search?resource_id=a083c865-6d60-4d1d-b6c6-b0c8a85f9c15').json;
+
+def TorontoMapApi(request):
+    response = requests.get(
+        'https://ckan0.cf.opendata.inter.prod-toronto.ca/ne/api/3/action/datastore_search?resource_id=a083c865-6d60-4d1d-b6c6-b0c8a85f9c15').json
     return render(request, 'template.html', {
         'title': 'api Test',
         'api': "geojson",
         'response': response
-})
+    })
 
-def Neighbourhoods (request):
+
+def Neighbourhoods(request):
     return render(request, "template.html", {
         "title": "List of Toronto Neighbourhoods",
     })
+
 
 def pivot(request):
     return render(request, 'pivot.html')
