fc9e329bea11d4a1395a84a94718f4d1e38c6f01
Estevan-C <estevancor@hotmail.com>
2021-10-21 10:36:15 -0400

    Update for detail content

--- DjangoHouse/Listing/urls.py
+++ DjangoHouse/Listing/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', views.Predict,name='predict')
+    path('', views.Predict,name='predict'),
+    path(r'^id/(?P<pk>\d+)$', views.detail, name='detail')
 ]
